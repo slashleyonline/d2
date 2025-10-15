810ab4beafb87d84703a09cd6b7366d2da59faf3
slashleyonline <dseward@ucsc.edu>
2025-10-15 17:10:55 +0000

    added simple green canvas 256x256

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
+
+const mainDiv = document.createElement("div");
+mainDiv.id = "mainDiv";
+
+document.body.appendChild(mainDiv);
+
+mainDiv.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const canvas = document.createElement("canvas");
+
+mainDiv.appendChild(canvas);
+
+const ctx = canvas.getContext("2d")!;
+
+ctx.fillStyle = "green";
+ctx.fillRect(0, 0, 256, 256);
