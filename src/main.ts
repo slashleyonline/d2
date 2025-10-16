7bf5c71ecafb024e2a84c8bc1e4b445a6ea28d5e
slashleyonline <dseward@ucsc.edu>
2025-10-16 21:34:22 +0000

    removed default content

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 const mainDiv = document.createElement("div");
 mainDiv.id = "mainDiv";
-
 document.body.appendChild(mainDiv);
-
-mainDiv.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
 
 const canvasDiv = document.createElement("div");
 canvasDiv.id = "canvasDiv";
-
 mainDiv.appendChild(canvasDiv);
 
 const canvas = document.createElement("canvas");
-
 canvas.id = "canvas";
-
 canvasDiv.appendChild(canvas);
+
 
 const ctx = canvas.getContext("2d")!;
 
